480043b05409ba69fd3a77c673e7207a7a25f774
denish-at <denish.satasiya@avestatechnologies.com>
2026-01-20 10:57:17 +0530

    Add missing InverseBoolConverter

--- DairyManagementSystem/src/DairyManagement.UI/Converters/ValueConverters.cs
+++ DairyManagementSystem/src/DairyManagement.UI/Converters/ValueConverters.cs
@@ -63,3 +63,20 @@ public class BoolToActiveConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+            return !boolValue;
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+            return !boolValue;
+        return false;
+    }
+}
